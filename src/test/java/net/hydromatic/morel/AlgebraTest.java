f236e1a86c96123799554c871a26c73460a297f6
Julian Hyde <jhyde@apache.org>
2020-11-15 17:50:44 -0800

    add test case

--- src/test/java/net/hydromatic/morel/AlgebraTest.java
+++ src/test/java/net/hydromatic/morel/AlgebraTest.java
@@ -44,6 +44,17 @@ public class AlgebraTest {
                 10));
   }
 
+  /** As previous, but with more concise syntax. */
+  @Test public void testScott2() {
+    final String ml = "from e in scott.emp yield e.deptno";
+    ml(ml)
+        .withBinding("scott", DataSet.SCOTT.foreignValue())
+        .assertType("int list")
+        .assertEvalIter(
+            equalsOrdered(20, 30, 30, 20, 30, 30, 10, 20, 10, 30, 20, 30, 20,
+                10));
+  }
+
   @Test public void testScottJoin() {
     final String ml = "let\n"
         + "  val emps = #emp scott\n"
